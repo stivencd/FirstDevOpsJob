96f9be5a5187ac8c7aab9592022a7adbd4273532
stivencd <30613134+stivencd@users.noreply.github.com>
2021-05-11 15:12:17 -0500

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -33,7 +33,7 @@ module "aforo255-cluster-scd" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "aforo255-cluster-scd"
   cluster_version = "1.17"
-  subnets         = ["subnet-adfa07f2", "subnet-a3c23d82"]  #CHANGE # Donot choose subnet from us-east-1e
+  subnets         = ["subnet-8586fea4", "subnet-886a5d86"]  #CHANGE # Donot choose subnet from us-east-1e
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
   #vpc_id         = "vpc-1234556abcdef"
